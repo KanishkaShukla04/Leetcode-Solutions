2aeec8313d295ef6ea9c6caff7152ec2c38069ff
Kanishka Shukla <kanishkashukla04@gmail.com>
2026-09-11 22:22:34 +0530

    Time: 5 ms (82.80%) | Memory: 46.3 MB (46.93%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        Set<Integer> seen=new HashSet<>();
+        int n=digits.length;
+
+        for(int h=0;h<n;h++){
+            if (digits[h]==0) continue;
+
+            for(int t=0;t<n;t++){
+                if (t==h) continue;
+                 
+                for(int u=0;u<n;u++){
+                    if(u==h||u==t) continue;
+
+                    if(digits[u]%2!=0) continue;
+
+                    int num=digits[h]*100 +digits[t]*10+digits[u];
+                    seen.add(num);
+                }
+            }
+        }
+        return seen.size();
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
